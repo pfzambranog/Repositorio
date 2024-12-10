b5fe7556ea5e9f562e36a8516adba01cde889283
pedrof <pfzambrano.gmail.com>
2024-12-10 10:55:17 -0600

    Adición inicial del proyecto

---
+++ Migracion/10.- Procedimientos/Procedimientos.bat
@@ -0,0 +1,1 @@
+osql -S %1 -d %2 -E  -e -i ".\10.- Procedimientos\1.- sp_ImportarPolizas.sql"   > mgrProcedimientos.log
---
+++ Migracion/8.- Saldos/saldos.bat
@@ -0,0 +1,7 @@
+osql -S %1 -d %2 -E  -e -i ".\8.- Saldos\1.- logCierreInicioEjercicioTbl.sql"    > Saldos.log
+osql -S %1 -d %2 -E  -e -i ".\8.- Saldos\2.- conParametrosGralesTbl.sql"        >> Saldos.log
+osql -S %1 -d %2 -E  -e -i ".\8.- Saldos\3.- Spp_generaAsientoCierreAnio.sql"   >> Saldos.log
+osql -S %1 -d %2 -E  -e -i ".\8.- Saldos\4.- Spp_actualizaSaldosMes.sql"        >> Saldos.log
+osql -S %1 -d %2 -E  -e -i ".\8.- Saldos\5.- Spp_actualizaInicioEjercicio.sql"  >> Saldos.log
+osql -S %1 -d %2 -E  -e -i ".\8.- Saldos\6.- Spp_InicioEjercicio.sql"           >> Saldos.log
+osql -S %1 -d %2 -E  -e -i ".\8.- Saldos\7.-Job FinalizaEjercicio.sql"          >> Saldos.log
